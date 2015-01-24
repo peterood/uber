f1aaf33a89ef8a9793444b9a3c9327808a1f59a0
Peter Rood <peter.rood@gmail.com>
2015-01-24 15:21:17 -0500

    make and style board

--- sudoku/js/main.js
+++ sudoku/js/main.js
@@ -1,1 +1,19 @@
+// Generate the board
+$('#board').html('');
+var board = '',
+	columns = 9,
+	rows = 9;
 
+board += '<table>';
+
+for (var i = 0; i < rows; i++) {
+	board += '<tr>';
+	for (var j = 0; j < columns; j++) {
+		board += '<td>' + '<input type="number" inputmode="numeric"></input>' + '</td>';
+	}
+	board += '</tr>';
+}
+
+board += '</table>';
+
+$('#board').append(board);
